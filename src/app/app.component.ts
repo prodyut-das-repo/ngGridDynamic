383778f4570c6bd65292448434a65454d25cfd9f
Prodyut Das <prodyut.das@capgemini.com>
2020-08-15 21:04:29 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'agGrid';
+  index: number;
+  color: string;
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  change(index: number, color: string) {
+    let b:HTMLElement = document.getElementsByClassName(`item${index - 1}`)[0] as HTMLElement;
+    b.style.cssText = `background-color: ${color}`;
+  }
 }
